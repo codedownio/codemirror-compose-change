b06ed1c5f782e6b099028b9a8e375faf09dc32ce
Tom McLaughlin <tom@codedown.io>
2023-06-18 21:22:38 -0700

    Updating rollup + plugins and more cleanups

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,24 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
 
-const pkg = require('./package.json')
+import camelCase from "lodash.camelcase";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 
-const libraryName = 'codemirror-compose-change'
+const libraryName = "codemirror-compose-change";
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: "dist/index.umd.js", name: camelCase(libraryName), format: "umd", sourcemap: true },
+    { file: "dist/index.es5.js", format: "es", sourcemap: true },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: "lodash")
   external: [
     // "lodash-es"
   ],
   watch: {
-    include: 'src/**',
+    include: "src/**",
   },
   plugins: [
     // Allow json resolution
@@ -29,12 +27,12 @@ export default {
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
+    // Allow node_modules resolution, so you can use "external" to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
+    // sourceMaps(),
   ],
-}
+};
